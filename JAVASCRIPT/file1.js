77f6ed36eb7c6a4aca002e7a5dbfe21e29ab288d
shiv578 <swarajrkfl22@gmail.com>
2026-06-25 10:36:22 +0530

    learning javascript

---
+++ JAVASCRIPT/file1.js
@@ -0,0 +1,11 @@
+console.log("Hello world");
+
+let num1 = 35;
+let num2 = 23;
+let sum = num1 + num2;
+// console.log("sum of two numbers are: " , sum);
+// console.log(`Num1 is ${num1} And value of num2 is ${num2} \n and sum of both are ${sum}`);
+const pi = 23;
+let ss = 3;
+let ss = 33;
+console.log(ss);
