a48b932ddd98ffabdb382d652bc2360c808d884a
shiv578 <swarajrkfl22@gmail.com>
2026-06-25 10:50:11 +0530

    learning javascript

--- JAVASCRIPT/file1.js
+++ JAVASCRIPT/file1.js
@@ -6,6 +6,22 @@ let sum = num1 + num2;
 // console.log("sum of two numbers are: " , sum);
 // console.log(`Num1 is ${num1} And value of num2 is ${num2} \n and sum of both are ${sum}`);
 const pi = 23;
-let ss = 3;
-let ss = 33;
-console.log(ss);
+// let ss = 3;
+// let ss = 33;
+// console.log(ss); this will give error
+
+
+// var ss = 3;
+// var ss = 33;
+// console.log(ss); this will not  give error
+
+// tdz = temproal dead zone   when let and const is hoisted into the memory mean that is begin accesed 
+// before the declaration then it give error both have hoisting but in let and conset it have the tdz that it cannot cross 
+// that line and give an error there
+
+// example
+// console.log(shivam);
+// var shivam = 4;
+// console.log(shivam);
+// this will work cause it dont meand var dont have the tdz but in let and const it will not work
+
