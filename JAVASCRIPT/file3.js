e9a51107f00be1eb6b5dae606d2adea0ab7c1e52
shiv578 <swarajrkfl22@gmail.com>
2026-06-26 09:40:32 +0530

    day 2 of js

---
+++ JAVASCRIPT/file3.js
@@ -0,0 +1,21 @@
+console.log("Hello world");
+console.log(typeof([]))
+console.log("100" - 3)
+console.log(typeof NaN)
+console.log(Boolean([]))
+
+// let arr1 = [1,2,3];
+// let arr2 = arr1;
+// arr2[0] = 9;
+// console.log(arr1[0])  this is getting the 9 because when we ar copyting it is copying the addres
+
+// let arr1 = [1,2,3];
+// let arr2 = [...arr1];
+// arr2[0] = 9;
+// console.log(arr1[0])  in this we are using the sperea operatit and it is making the copying of that array new array it si maong
+
+console.log(typeof {})
+ let a = String(123); //// 
+ console.log(typeof a) // output is string typecasting
+  console.log(parseInt("12abc")) // output is 12 
+    console.log(parseInt("12abc"))
