2b55a45936a928a2937db615134b44526c18dd87
shiv578 <swarajrkfl22@gmail.com>
2026-06-30 09:56:21 +0530

    learning callback funciton

--- JAVASCRIPT/file6.js
+++ JAVASCRIPT/file6.js
@@ -9,3 +9,20 @@ function done(){
 greet("shivam", done)  // this is call back function
 
 //call back help
+
+login(function () {
+    getUser(function () {
+        getOrders(function () {
+            getPayment(function () {
+                sendEmail(function () {
+                    logout(function () {
+                        console.log("Done");
+                    });
+                });
+            });
+        });
+    });
+});  // it is a nested CALL BACK FUNCITON IN THAT CALLLING KEEPS CALLING ANOTHER FUNCTION THAT MAKES'
+ // THE CODE HAED TO READ HARD TO MAIN TAIN HARD TO DEBUG AND ERROR HANDLING ALSO TIOUGH THIS IS CALLED  CALLBACK HELL;
+
+ 
