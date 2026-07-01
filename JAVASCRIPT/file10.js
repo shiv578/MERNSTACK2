354d0390ec5b14dea580fd525e18e9a5b3d3c7c7
shiv578 <swarajrkfl22@gmail.com>
2026-07-01 10:30:09 +0530

    learning object in javascript

---
+++ JAVASCRIPT/file10.js
@@ -0,0 +1,16 @@
+const obj1 = {
+    getfullname(){
+
+    }
+}
+
+const obj2 = {
+    age: 34,
+    occupation : "Frontend eng",
+    getage(){
+        return `Age: ${this.age}`
+    },
+    getoccupation(){
+        return `Occupatio: ${this.occupation}`
+    }
+}
