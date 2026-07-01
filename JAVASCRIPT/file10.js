14c5f3009f658149597ac3351295f1c788d921d8
shiv578 <swarajrkfl22@gmail.com>
2026-07-01 11:06:33 +0530

    learning object in javascript

--- JAVASCRIPT/file10.js
+++ JAVASCRIPT/file10.js
@@ -1,16 +1,27 @@
+console.log("hello...");
+
 const obj1 = {
-    getfullname(){
+    fname: "Shivam",
+    lname: "Swaraj",
 
+    getfullname() {
+        return `${this.fname} ${this.lname}`;
     }
-}
-
+};
+// This is a common pattern in JavaScript: objects contain both data (properties) and behavior (methods).
 const obj2 = {
     age: 34,
-    occupation : "Frontend eng",
-    getage(){
-        return `Age: ${this.age}`
+    occupation: "Frontend Eng",
+
+    getage() {
+        return `Age: ${this.age}`;
     },
-    getoccupation(){
-        return `Occupatio: ${this.occupation}`
+
+    getoccupation() {
+        return `Occupation: ${this.occupation}`;
     }
-}
+};
+
+console.log(obj1.getfullname());
+console.log(obj2.getage());
+console.log(obj2.getoccupation());
---
+++ JAVASCRIPT/file11.js
@@ -0,0 +1,21 @@
+console.log("he");
+//learning array in javascritp
+const arr = [4,5,4,3];
+console.log(arr);
+
+
+console.log("Second concept");
+
+const arr1 = new Array(5);
+
+for(let i = 0 ; i < arr1.length ; i++){
+arr1[i] = i;
+}
+console.log(arr1);
+
+
+
+/// 
+console.log("Now learning the global execuiton ");
+
+
