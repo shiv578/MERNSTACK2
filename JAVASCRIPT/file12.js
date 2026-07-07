7cf6b44bcd6f97fc909e3ffc1a61497b2c752776
shiv578 <swarajrkfl22@gmail.com>
2026-07-07 09:50:14 +0530

    learning debouncing

--- JAVASCRIPT/file12.js
+++ JAVASCRIPT/file12.js
@@ -1,9 +1,33 @@
+
+//debouncing
+function debounce(callback , delay){
+let timerid;
+return function(...args){
+    clearTimeout(timerid);
+  timerid =   setTimeout(() =>{
+  callback(...args);
+         
+    },delay * 1000);
+}
+
+
+}
 const search = (query) =>{
 console.log("searching for " , query);
 
 }
 
-search("he");
-search("hello");
-search("hello ");
-search("hello world");
+
+const searchusingdebounce = debounce(search, 2);
+
+
+
+// this is devounce function that we are making we use this
+//cause to make the seacjing more efficient and lower the api cal on 
+//each letter 
+
+
+searchusingdebounce("he");
+searchusingdebounce("hello");
+searchusingdebounce("hello ");
+searchusingdebounce("hello world");
